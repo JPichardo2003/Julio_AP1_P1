9b224efca9eaf22ca6c4c44821eb52703602d077
JPichardo2003 <julio_pichardo@ucne.edu.do>
2023-09-25 19:47:14 -0400

    Inyectando Context en el Program

---
+++ DAL/Context.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace Julio_AP1_P1.DAL
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options) { }
+    }
+}
