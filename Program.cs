f4f3f89fc2a70b1cc9fccbffab6e65b6b80e34a8
JPichardo2003 <julio_pichardo@ucne.edu.do>
2023-09-25 20:50:03 -0400

    Agregando NotificationService

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.EntityFrameworkCore;
 using Julio_AP1_P1.DAL;
+using Radzen;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,6 +13,8 @@ builder.Services.AddServerSideBlazor();
 var ConStr = builder.Configuration.GetConnectionString("ConStr");
 
 builder.Services.AddDbContext<Context>(op => op.UseSqlite(ConStr));
+
+builder.Services.AddScoped<NotificationService>();
 
 var app = builder.Build();
 
