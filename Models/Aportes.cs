0cd9db379b08782cff7d94efc1b737d41da5d6a5
JPichardo2003 <julio_pichardo@ucne.edu.do>
2023-10-02 18:36:05 -0400

    Añadiendo el Modelo al Proyecto

---
+++ Models/Aportes.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Julio_AP1_P1.Models
+{
+    public class Aportes
+    {
+        [Key]
+        public int AporteId { get; set; }
+
+        [Required(ErrorMessage = "Campo Obligatorio")]
+        public DateTime Fecha { get; set; }
+
+        [Required(ErrorMessage = "Campo Obligatorio")]
+        [RegularExpression(@"^[a-zA-Z]+$",ErrorMessage ="Este campo no acepta digitos")]
+        public string? Persona { get; set; }
+
+        [Required(ErrorMessage = "Campo Obligatorio")]
+        public string? Observacion { get; set; }
+
+        [Required(ErrorMessage = "Campo Obligatorio")]
+        [Range(1,float.MaxValue, ErrorMessage ="Este Campo no puede ser <=0")]
+        public float Monto { get; set; }
+
+    }
+}
