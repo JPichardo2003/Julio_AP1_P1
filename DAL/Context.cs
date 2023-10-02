46465ee80e02f9b9b53952a302b60e2d9725c0a1
JPichardo2003 <julio_pichardo@ucne.edu.do>
2023-10-02 18:39:48 -0400

    Agregando linea de codigo Dbset<Aportes> aportes al contexto

--- DAL/Context.cs
+++ DAL/Context.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Julio_AP1_P1.Models;
+using Microsoft.EntityFrameworkCore;
 namespace Julio_AP1_P1.DAL
 {
     public class Context : DbContext
     {
         public Context(DbContextOptions<Context> options) : base(options) { }
+        public DbSet<Aportes> Aportes { get; set;}
     }
 }
