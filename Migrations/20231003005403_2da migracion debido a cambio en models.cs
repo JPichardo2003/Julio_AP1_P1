4978fcd0ce7b5386c8665a70c54efa79834ebd0b
JPichardo2003 <julio_pichardo@ucne.edu.do>
2023-10-02 21:39:59 -0400

    Nueva Migracion y BD debido a cambios en models

---
+++ Migrations/20231003005403_2da migracion debido a cambio en models.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Julio_AP1_P1.Migrations
+{
+    /// <inheritdoc />
+    public partial class _2damigraciondebidoacambioenmodels : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Observacion",
+                table: "Aportes",
+                newName: "Observación");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Observación",
+                table: "Aportes",
+                newName: "Observacion");
+        }
+    }
+}
